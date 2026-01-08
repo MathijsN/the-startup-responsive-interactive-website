96babf0a2d3247c30dae50988c1530876efb6880
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-01-08 15:34:38 +0100

    file cleanup

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-// const reservationSetting = document.getElementById('reservation-settings');
-// const cancelationSetting = document.getElementById('cancelation-settings');
-// const reminderSetting = document.getElementById('reminder-settings');
-
-// reservationSetting.addEventListener("command", (e) => {
-//     if (e.command === "--toggle-settings") {
-//         reservationSetting.toggleAttribute('disabled')
-//     }
-// })
-// cancelationSetting.addEventListener("command", (e) => {
-//     if (e.command === "--toggle-settings") {
-//         cancelationSetting.toggleAttribute('disabled')
-//     }
-// })
-// reminderSetting.addEventListener("command", (e) => {
-//     if (e.command === "--toggle-settings") {
-//         reminderSetting.toggleAttribute('disabled')
-//     }
-// })
-
-
 window.addEventListener('command', function(ev) {
-  console.log(ev);
-  console.log(ev.command);
-  console.log(ev.source);
   console.log(ev.target);
 
   ev.target.toggleAttribute('disabled')
 
+  
 
 }, {capture: true});
