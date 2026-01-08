4c871c41c24f9534a21c3090997c6232b65d3217
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-01-08 15:11:28 +0100

    ev.target #2

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-const reservationSetting = document.getElementById('reservation-settings');
-const cancelationSetting = document.getElementById('cancelation-settings');
-const reminderSetting = document.getElementById('reminder-settings');
+// const reservationSetting = document.getElementById('reservation-settings');
+// const cancelationSetting = document.getElementById('cancelation-settings');
+// const reminderSetting = document.getElementById('reminder-settings');
 
-reservationSetting.addEventListener("command", (e) => {
-    if (e.command === "--toggle-settings") {
-        reservationSetting.toggleAttribute('disabled')
-    }
-})
-cancelationSetting.addEventListener("command", (e) => {
-    if (e.command === "--toggle-settings") {
-        cancelationSetting.toggleAttribute('disabled')
-    }
-})
-reminderSetting.addEventListener("command", (e) => {
-    if (e.command === "--toggle-settings") {
-        reminderSetting.toggleAttribute('disabled')
-    }
-})
+// reservationSetting.addEventListener("command", (e) => {
+//     if (e.command === "--toggle-settings") {
+//         reservationSetting.toggleAttribute('disabled')
+//     }
+// })
+// cancelationSetting.addEventListener("command", (e) => {
+//     if (e.command === "--toggle-settings") {
+//         cancelationSetting.toggleAttribute('disabled')
+//     }
+// })
+// reminderSetting.addEventListener("command", (e) => {
+//     if (e.command === "--toggle-settings") {
+//         reminderSetting.toggleAttribute('disabled')
+//     }
+// })
+
+
+window.addEventListener('command', function(ev) {
+  console.log(ev);
+  console.log(ev.command);
+  console.log(ev.source);
+  console.log(ev.target);
+
+  ev.target.toggleAttribute('disabled')
+
+
+}, {capture: true});
