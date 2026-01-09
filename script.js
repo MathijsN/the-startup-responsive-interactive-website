9dd08dca66c754c68b9b4afca7f2bb45cc7e8c82
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-01-09 12:41:33 +0100

    fixed safari fallback #2

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-// console.log(Object.getPrototypeOf(window.CommandEvent))
-// Event()
+const buttons = document.querySelectorAll('.trigger-list')
 
-// if (!('CommandEvent' in window)) {
-    
-//     Event.prototype.myCommandEvent = function(callbackFunction) {
-//     }
-// }
+if (!('CommandEvent' in window)) {
+
+    buttons.forEach(button => {
+        button.addEventListener('click', (ev) => {
+            const fieldset = ev.target.closest("fieldset")
+
+            console.log(ev.target.closest("fieldset"))
+            fieldset.toggleAttribute("disabled")
+        })
+    })
+
+}
 
 window.addEventListener('command', function (ev) {
     console.log(ev.target);
 
     ev.target.toggleAttribute('disabled')
-
 }, { capture: true });
- 
