ebea5d0b3cc22ef7a1c8afe2e6ee10bb323c253a
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-01-12 18:01:29 +0100

    required input logic #12

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const saveSettingsButtonSpan = document.querySelector('.save-button span')
 const saveSettingsButtonSvg = document.querySelector('.save-button svg')
 const form = document.querySelector('.settings-form')
 const formSubmit = document.querySelector('.save-button')
+const inputs = document.querySelectorAll('input[type="checkbox"][required]')
+
+let validityArr = []
 
 if (!('CommandEvent' in window)) {
 
     buttons.forEach(button => {
         button.addEventListener('click', (ev) => {
+
             const fieldset = ev.target.closest("fieldset")
 
-            console.log(ev.target.closest("fieldset"))
             fieldset.toggleAttribute("disabled")
         })
     })
@@ -19,7 +22,6 @@ if (!('CommandEvent' in window)) {
 } else {
 
     window.addEventListener('command', function (ev) {
-        console.log(ev.target);
 
         ev.target.toggleAttribute('disabled')
     }, { capture: true });
@@ -30,21 +32,33 @@ if (!('CommandEvent' in window)) {
 
 form.addEventListener('submit', (ev) => {
 
-    if (!form.checkValidity()) {
-        formSubmit.setAttribute("disabled")
-    }
-    else {
-        formSubmit.removeAttribute("disabled")
+    if (validityArr.every(true)) {
 
         saveSettingsButtonSpan.classList.add('save-animation')
         saveSettingsButtonSvg.classList.add('save-animation')
-
+        formSubmit.removeAttribute('disabled')
+    
         setTimeout(() => {
-
+    
             saveSettingsButtonSpan.classList.remove('save-animation')
             saveSettingsButtonSvg.classList.remove('save-animation')
         }, 2000)
-
+    
         ev.preventDefault()
+    } else {
+        formSubmit.setAttribute('disabled')
     }
+
 })
+
+inputs.forEach(input => {
+        input.addEventListener('change', () => {
+            console.log(validityArr)
+
+            inputs.forEach(input => {
+
+                validityArr.push(input.checkValidity())
+            })
+        validityArr = []
+        })
+    })
