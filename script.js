e9df8877ccba0873524dc2cc7123b364d8a55e74
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-01-12 12:25:35 +0100

    Created button #12

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const buttons = document.querySelectorAll('.trigger-list')
+const saveSettingsButton = document.querySelector('.save-button')
+const saveSettingsButtonSpan = document.querySelector('.save-button span')
+const saveSettingsButtonSvg = document.querySelector('.save-button svg')
+const form = document.querySelector('.settings-form')
+const formSubmit = document.querySelector('.save-button')
 
 if (!('CommandEvent' in window)) {
 
@@ -11,10 +16,30 @@ if (!('CommandEvent' in window)) {
         })
     })
 
+} else {
+
+    window.addEventListener('command', function (ev) {
+        console.log(ev.target);
+
+        ev.target.toggleAttribute('disabled')
+    }, { capture: true });
+
 }
 
-window.addEventListener('command', function (ev) {
-    console.log(ev.target);
 
-    ev.target.toggleAttribute('disabled')
-}, { capture: true });
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+    console.log("submitted")
+    if (!form.checkValidity()) {
+        formSubmit.setAttribute("disabled")
+
+    }
+    else {
+        formSubmit.removeAttribute("disabled")
+        saveSettingsButtonSpan.classList.add('save-animation')
+        saveSettingsButtonSvg.classList.add('save-animation')
+
+        ev.preventDefault()
+    }
+})
